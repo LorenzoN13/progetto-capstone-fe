cbbca3e76c9cbcc9b53e9c2865cb03d474028512
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-03-24 12:51:10 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/log-system/login/login.component';
+import { RegisterComponent } from './pages/log-system/register/register.component';
+import { CreateComponent } from './pages/create/create.component';
+import { EditComponent } from './pages/edit/edit.component';
+import { Pages404Component } from './pages/pages404/pages404.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { EditProfileComponent } from './pages/user-profile/edit-profile/edit-profile.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreateComponent,
+    EditComponent,
+    Pages404Component,
+    UserProfileComponent,
+    EditProfileComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
